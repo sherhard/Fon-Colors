45410ad5c88c73de56cc54eca7475a1f5ca3556e
sherhard <goeasysher@gmail.com>
2023-02-19 00:16:45 +0800

    add new code

--- color.js
+++ color.js
@@ -6,3 +6,11 @@ let colors=document.getElementsByClassName('colors');
 		function onColorFon(){
 			document.body.style.background=this.value;
 		}
+
+let colors_fonBtn=document.getElementById('colors_fonBtn');
+let all_colos=document.getElementById('all_colors');
+
+colors_fonBtn.addEventListener('click',fon);
+	function fon(){
+		document.body.style.background=all_colos.value;
+	}
